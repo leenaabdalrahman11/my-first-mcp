49952f3099c11281930aaca36d7fb403dafb6451
aseelsaleh123 <salehaseel129@gmail.com>
2026-07-29 22:28:22 +0300

    create update job and get job tools

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-
+import { registerUpdateJobStatusTool } from "./tools/update_job_status.js";
 import { registerAddJobTool } from "./tools/add-job.js";
 import { registerListJobsTool } from "./tools/list_jobs.js";
 import { registerSearchJobsTool } from "./tools/search_jobs.js";
-
+import { registerGetJobTool } from "./tools/get_job.js";
 const server = new McpServer({
   name: "my-first-mcp",
   version: "0.1.0",
@@ -13,7 +13,8 @@ const server = new McpServer({
 registerAddJobTool(server);
 registerListJobsTool(server);
 registerSearchJobsTool(server);
-
+registerUpdateJobStatusTool(server);
+registerGetJobTool(server);
 const transport = new StdioServerTransport();
 
 await server.connect(transport);
